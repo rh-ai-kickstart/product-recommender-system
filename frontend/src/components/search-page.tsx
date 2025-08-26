df1c6aedd6d19e01aa94a6d958a37a9df8e4ab8c
Hadar Cohen <hacohen@redhat.com>
2025-08-26 08:59:54 +0300

    add search by image link and url to the frontend

--- frontend/src/components/search-page.tsx
+++ frontend/src/components/search-page.tsx
@@ -1,6 +1,7 @@
-import { Skeleton, Title } from '@patternfly/react-core';
+import { Skeleton, Title, Divider } from '@patternfly/react-core';
 import { GalleryView } from './Gallery';
 import { usePersonalizedRecommendations } from '../hooks/useRecommendations';
+import { ImageSearch } from './ImageSearch';
 
 export function SearchPage() {
   const { data, isError, isLoading } = usePersonalizedRecommendations();
@@ -10,6 +11,14 @@ export function SearchPage() {
       <Title headingLevel={'h1'} style={{ paddingBottom: 20 }}>
         Search
       </Title>
+
+      {/* Add Image Search Component */}
+      <div style={{ marginBottom: 20 }}>
+        <ImageSearch />
+      </div>
+
+      <Divider style={{ marginBottom: 20 }} />
+
       {isLoading ? (
         <Skeleton style={{ height: 200 }} />
       ) : isError ? (
--- frontend/src/components/search.tsx
+++ frontend/src/components/search.tsx
@@ -1,9 +1,18 @@
 import { useState } from 'react';
-import { SearchInput } from '@patternfly/react-core';
+import {
+  SearchInput,
+  Tabs,
+  Tab,
+  TabTitleText,
+  TabContent,
+  TabContentBody,
+} from '@patternfly/react-core';
 import { useNavigate } from '@tanstack/react-router';
+import { ImageSearch } from './ImageSearch';
 
 export const Search: React.FunctionComponent = () => {
   const [value, setValue] = useState('');
+  const [activeTabKey, setActiveTabKey] = useState<string | number>(0);
   const navigate = useNavigate();
 
   const onChange = (value: string) => {
@@ -16,14 +25,39 @@ export const Search: React.FunctionComponent = () => {
     }
   };
 
+  const handleTabClick = (_: any, tabIndex: string | number) => {
+    setActiveTabKey(tabIndex);
+  };
+
   return (
-    <SearchInput
-      placeholder='Find a product'
-      value={value}
-      onChange={(_event, value) => onChange(value)}
-      onSearch={onSearch}
-      onClear={() => onChange('')}
-      className='pf-v6-u-w-100'
-    />
+    <Tabs
+      activeKey={activeTabKey}
+      onSelect={handleTabClick}
+      isBox
+      aria-label='Search options'
+      style={{ marginBottom: '20px' }}
+    >
+      <Tab eventKey={0} title={<TabTitleText>Text Search</TabTitleText>}>
+        <TabContent id='text-search-tab'>
+          <TabContentBody>
+            <SearchInput
+              placeholder='Find a product'
+              value={value}
+              onChange={(_event, value) => onChange(value)}
+              onSearch={onSearch}
+              onClear={() => onChange('')}
+              className='pf-v6-u-w-100'
+            />
+          </TabContentBody>
+        </TabContent>
+      </Tab>
+      <Tab eventKey={1} title={<TabTitleText>Image URL Search</TabTitleText>}>
+        <TabContent id='image-search-tab'>
+          <TabContentBody>
+            <ImageSearch />
+          </TabContentBody>
+        </TabContent>
+      </Tab>
+    </Tabs>
   );
 };
