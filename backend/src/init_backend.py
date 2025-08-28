5a5cac915f007f559c77d3b08e05a6a367cf5875
ori346 <64233027+ori346@users.noreply.github.com>
2025-08-28 15:45:26 +0300

    Update style init_backend.py
    Signed-off-by: ori346 <64233027+ori346@users.noreply.github.com>

--- backend/src/init_backend.py
+++ backend/src/init_backend.py
@@ -5,26 +5,28 @@ This should be run by a job once per cluster.
 
 import asyncio
 import logging
-import subprocess
-
-from database.db import get_engine, get_db
-from database.fetch_feast_users import seed_users
-from database.models_sql import Base, Category
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
-import pandas as pd
 import os
-from sqlalchemy import select
+import subprocess
+import uuid
 from collections import deque
 from dataclasses import dataclass
-import uuid
+
+import pandas as pd
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+from database.db import get_db, get_engine
+from database.fetch_feast_users import seed_users
+from database.models_sql import Base, Category
 
 logger = logging.getLogger(__name__)
 
+
 @dataclass
 class category_dc:
-   category_id:uuid
-   name: str
-   parent_id: uuid
+    category_id: uuid
+    name: str
+    parent_id: uuid
 
 
 async def create_tables():
@@ -42,24 +44,36 @@ async def create_tables():
 
 async def populate_categories():
     try:
-        #Read parquet file containing categories in Category, Parent Category format
+        # Read parquet file containing categories in Category, Parent Category format
         raw_categories_file = "/app/recommendation-core/src/recommendation_core/feature_repo/data/category_relationships.parquet"
         df = pd.read_parquet(raw_categories_file)
 
         engine = get_engine()
         SessionLocal = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
-        root_categories_df = df[df['Parent Category'].isnull()]
-        root_categories = [category_dc(uuid.uuid4(), row['Category'], None) for _, row in root_categories_df.iterrows()]
+        root_categories_df = df[df["Parent Category"].isnull()]
+        root_categories = [
+            category_dc(uuid.uuid4(), row["Category"], None)
+            for _, row in root_categories_df.iterrows()
+        ]
         q = deque(root_categories)
 
         async with SessionLocal() as session:
-            while (len(q)):
-                 next_category = q.popleft()
-                 children_of_next_df = df[df['Parent Category'] == next_category.name]
-                 children_of_next = [category_dc(uuid.uuid4(), row['Category'], next_category.category_id) for _, row in children_of_next_df.iterrows()]
-                 q.extend(children_of_next)
-                 session.add(Category(category_id=next_category.category_id, name=next_category.name, parent_id=next_category.parent_id))
+            while len(q):
+                next_category = q.popleft()
+                children_of_next_df = df[df["Parent Category"] == next_category.name]
+                children_of_next = [
+                    category_dc(uuid.uuid4(), row["Category"], next_category.category_id)
+                    for _, row in children_of_next_df.iterrows()
+                ]
+                q.extend(children_of_next)
+                session.add(
+                    Category(
+                        category_id=next_category.category_id,
+                        name=next_category.name,
+                        parent_id=next_category.parent_id,
+                    )
+                )
 
             await session.commit()
     except Exception as e:
@@ -82,6 +96,7 @@ async def setup_all():
         logger.info("🔄 Keeping pod alive for debugging...")
         # Keep the pod running for debugging
         subprocess.run(["tail", "-f", "/dev/null"])
+
 
 if __name__ == "__main__":
     asyncio.run(setup_all())
